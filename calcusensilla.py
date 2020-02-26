ffc30700254f1811b4cf54ee4c058f51228a7344
kvnpolo <kvnpolo97@gmail.com>
2020-02-26 14:50:32 -0500

    probando

---
+++ calcusensilla.py
@@ -0,0 +1,14 @@
+n1=float(input("ingrese el primer numero"))
+n2=float(input("ingrese el segundo numero"))
+suma = n1 + n2
+resta = n1 - n2
+multiplicacion = n1 * n2
+division = n1 / n2
+suma=str(suma)
+print("El resultado de la suma es: " + suma)
+resta=str(resta)
+print("El resultado de la resta es: ", resta)
+
+print("El resultado de la multiplicacion es: ", multiplicacion)
+
+print("El resultado de la division es: ", division)
